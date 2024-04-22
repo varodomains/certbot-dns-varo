7adc79403852d2b62d04abf614f428e3243aabf1
shakestation <hshub@hshub.io>
2024-04-22 03:48:47 +0000

    add to ignore

--- setup.py
+++ setup.py
@@ -2,14 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='certbot-dns-varo',
-    version='0.1.0',
-    description='Certbot plugin for Varo',
+    version='1.0.0',
+    description='A Certbot plugin for Varo',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/varodomains/certbot-dns-varo',
+    author='eskimo',
+    author_email='me@eskimo.dev',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'certbot>=1.1.0',
+        'certbot',
         'requests',
         'zope.interface',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
     ],
     entry_points={
         'certbot.plugins': [
